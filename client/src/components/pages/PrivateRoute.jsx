dd37a7132f2c6fd2b925c0a187f0e91fd62f8dca
Butko Denys <Butko.dv@gmail.com>
2024-08-16 10:15:29 +0000

    Update PrivateRoute component to use "element" prop instead of "children" prop

--- client/src/components/pages/PrivateRoute.jsx
+++ client/src/components/pages/PrivateRoute.jsx
@@ -8,8 +8,8 @@ const isAuthenticated = () => {
   return !!token;
 };
 
-const PrivateRoute = ({ children }) => {
-  return isAuthenticated() ? children : <Navigate to="/login" />;
+const PrivateRoute = ({ element }) => {
+  return isAuthenticated() ? element : <Navigate to="/login" />;
 };
 
 export default PrivateRoute;
