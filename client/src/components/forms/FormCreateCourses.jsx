a4c98a9fd1a718c528ecd9f19ffc97ce02782f8b
Denis Butko <butko.dv@gmail.com>
2024-08-26 21:53:03 +0300

    feat: Update npm dependencies to include @mui/x-date-pickers and dayjs
    Update the npm dependencies in the client/package.json and client/package-lock.json files to include the latest versions of @mui/x-date-pickers and dayjs. This update ensures that the project uses the most up-to-date versions of these libraries, which may include bug fixes, performance improvements, and new features.
    
    Recent repository commits:
    - feat: Refactor FormCreateCourses component to use MUI components and react-hook-form
    - refactor: Rename FormCourses component to FormCreateCourses
    - refactor: Rename FormCourses component to FormCreateCourses
    - feat: Add FormCourses component for creating new courses
    - feat: Fix typo in Courses page import
    - feat: Add validation for creating new courses
    - feat: Add Courses model and routes
    - feat: Add Contracts page and update navigation
    - feat: Add Company page and update navigation

--- client/src/components/forms/FormCreateCourses.jsx
+++ client/src/components/forms/FormCreateCourses.jsx
@@ -8,6 +8,10 @@ import {
   TextField,
 } from '@mui/material';
 
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -26,99 +30,130 @@ export default function FormCreateCourses({ handleClose }) {
   };
 
   return (
-    <Box
-      component={'form'}
-      onSubmit={handleSubmit(onSubmit)}
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        width: 600,
-        padding: 4,
-        backgroundColor: 'white',
-      }}
-    >
-      <Controller
-        name="courseType"
-        control={control}
-        defaultValue=""
-        render={({ field }) => (
-          <FormControl variant="outlined" fullWidth margin="normal">
-            <InputLabel id="courseType">Тип курса</InputLabel>
-            <Select {...field} labelId="courseType" label="Course Type">
-              <MenuItem value="Тематичне удосконалення">
-                Тематичне удосконалення
-              </MenuItem>
-              <MenuItem value="Спеціалізація">Спеціалізація</MenuItem>
-            </Select>
-          </FormControl>
-        )}
-      />
-      <Controller
-        name="courseName"
-        control={control}
-        defaultValue=""
-        render={({ field }) => (
-          <TextField
-            {...field}
-            label="Назва курсу"
-            variant="outlined"
-            fullWidth
-            margin="normal"
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <Box
+        component={'form'}
+        onSubmit={handleSubmit(onSubmit)}
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          width: 600,
+          padding: 4,
+          backgroundColor: 'white',
+        }}
+      >
+        <Controller
+          name="courseType"
+          control={control}
+          defaultValue=""
+          render={({ field }) => (
+            <FormControl variant="outlined" fullWidth margin="normal">
+              <InputLabel id="courseType">Тип курса</InputLabel>
+              <Select {...field} labelId="courseType" label="Course Type">
+                <MenuItem value="Тематичне удосконалення">
+                  Тематичне удосконалення
+                </MenuItem>
+                <MenuItem value="Спеціалізація">Спеціалізація</MenuItem>
+              </Select>
+            </FormControl>
+          )}
+        />
+        <Controller
+          name="courseName"
+          control={control}
+          defaultValue=""
+          render={({ field }) => (
+            <TextField
+              {...field}
+              label="Назва курсу"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+            />
+          )}
+        />
+        <Controller
+          name="coursePrice"
+          control={control}
+          defaultValue=""
+          render={({ field }) => (
+            <TextField
+              {...field}
+              label="Вартість курсу"
+              variant="outlined"
+              type="number"
+              fullWidth
+              margin="normal"
+              inputProps={{ min: 0, step: 100 }}
+            />
+          )}
+        />
+        <Controller
+          name="coursePoints"
+          control={control}
+          defaultValue=""
+          render={({ field }) => (
+            <TextField
+              {...field}
+              label="Бали"
+              variant="outlined"
+              type="number"
+              fullWidth
+              margin="normal"
+              inputProps={{ min: 0, step: 1 }}
+            />
+          )}
+        />
+        <Controller
+          name="courseDepartment"
+          control={control}
+          defaultValue=""
+          render={({ field }) => (
+            <TextField
+              {...field}
+              label="Кафедра"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+            />
+          )}
+        />
+        <Box sx={{ display: 'flex', gap: 2, width: '100%', marginY: 2 }}>
+          <Controller
+            name="courseDateStart"
+            control={control}
+            defaultValue={null}
+            render={({ field }) => (
+              <DatePicker
+                {...field}
+                label="Дата початку"
+                inputFormat="YYYY-MM-DD"
+                clearable
+                fullWidth
+              />
+            )}
           />
-        )}
-      />
-      <Controller
-        name="coursePrice"
-        control={control}
-        defaultValue=""
-        render={({ field }) => (
-          <TextField
-            {...field}
-            label="Вартість курсу"
-            variant="outlined"
-            type="number"
-            fullWidth
-            margin="normal"
-            inputProps={{ min: 0, step: 100 }}
+          <Controller
+            name="courseDateEnd"
+            control={control}
+            defaultValue={null}
+            render={({ field }) => (
+              <DatePicker
+                {...field}
+                label="Дата закінчення"
+                inputFormat="YYYY-MM-DD"
+                clearable
+                fullWidth
+              />
+            )}
           />
-        )}
-      />
-      <Controller
-        name="coursePoints"
-        control={control}
-        defaultValue=""
-        render={({ field }) => (
-          <TextField
-            {...field}
-            label="Бали"
-            variant="outlined"
-            type="number"
-            fullWidth
-            margin="normal"
-            inputProps={{ min: 0, step: 1 }}
-          />
-        )}
-      />
-      <Controller
-        name="courseDepartment"
-        control={control}
-        defaultValue=""
-        render={({ field }) => (
-          <TextField
-            {...field}
-            label="Кафедра"
-            variant="outlined"
-            fullWidth
-            margin="normal"
-          />
-        )}
-      />
-
-      <Button type="submit" variant="contained" color="primary">
-        Add Course
-      </Button>
-      <ToastContainer />
-    </Box>
+        </Box>
+        <Button type="submit" variant="contained" color="primary">
+          Додати курс
+        </Button>
+        <ToastContainer />
+      </Box>
+    </LocalizationProvider>
   );
 }
