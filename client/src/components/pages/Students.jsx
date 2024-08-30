0e9bd6e07127a80a3fccb11b536680dc1abcb234
Butko Denys <Butko.dv@gmail.com>
2024-08-30 09:00:11 +0000

    оновлено сторінку студентів з новим дизайном і модальним вікном

--- client/src/components/pages/Students.jsx
+++ client/src/components/pages/Students.jsx
@@ -1,13 +1,39 @@
-import { Typography } from '@mui/material';
+import { Box, Typography, Button, Modal } from '@mui/material';
 
 const Students = () => {
   return (
-    <div>
-      <h1>Students</h1>
-      <Typography variant="body1">
-        тут будет таблица со студентами и их данными
-      </Typography>
-    </div>
+    <Box
+      sx={{
+        backgroundColor: 'purple',
+      }}
+    >
+      <h1>Студенти</h1>
+      <Button variant="contained" color="primary">
+        Додати студента
+      </Button>
+      <Modal open={true}>
+        <Box
+          sx={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            width: 400,
+            bgcolor: 'background.paper',
+            border: '2px solid #000',
+            boxShadow: 24,
+            p: 4,
+          }}
+        >
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
+    </Box>
   );
 };
 
